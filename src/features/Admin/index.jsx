fc96792d303051bc593062fd95771f654752be8a
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-07-09 17:02:33 +0700

    Set up admin component

--- src/features/Admin/index.jsx
+++ src/features/Admin/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 
 AdminFeature.propTypes = {};
 
 function AdminFeature(props) {
-  return <div>This is a Dashboard</div>;
+  const match = useRouteMatch();
+  return (
+    <div>
+      <Switch>
+        <Route path="" />
+        <Route />
+      </Switch>
+    </div>
+  );
 }
 
 export default AdminFeature;
