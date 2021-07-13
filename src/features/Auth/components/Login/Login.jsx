9f1f305195e77447b2bb39cec45dd4e9de0bd46f
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-07-13 09:34:18 +0700

    Redirect to dashboard page after login successfully

--- src/features/Auth/components/Login/Login.jsx
+++ src/features/Auth/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import { login } from 'features/Auth/authSlice';
 import { useSnackbar } from 'notistack';
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import LoginForm from '../LoginForm/LoginForm';
 
 Login.propTypes = {};
@@ -21,15 +22,23 @@ function Login(props) {
   const classes = useStyles();
   const dispatch = useDispatch();
   const { enqueueSnackbar } = useSnackbar();
+  const history = useHistory();
 
   const handleSubmit = async (values) => {
     try {
       const action = login(values);
       const resultAction = await dispatch(action);
       unwrapResult(resultAction);
+
+      enqueueSnackbar('Login successfully! 😍😎', {
+        variant: 'success',
+        anchorOrigin: { vertical: 'top', horizontal: 'right' },
+      });
+
+      history.push('/admin');
     } catch (error) {
       console.log('Failed to login: ', error);
-      enqueueSnackbar(error.message, {
+      enqueueSnackbar(`${error.message} 😥😭`, {
         variant: 'error',
         anchorOrigin: { vertical: 'top', horizontal: 'right' },
       });
