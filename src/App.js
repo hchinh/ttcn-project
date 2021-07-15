2850869bcb5b29e96af35d9f5c83449bce4fef1a
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-07-15 11:24:02 +0700

    fix ui

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ import ErrorPage from './components/ErrorPage/ErrorPage';
 import Product from './features/Product/product.jsx';
 import ProductDetail from './features/ProductDetail/ProductDetail';
 
-
 function App() {
   return (
     <div>
       <Switch>
         <Route path="/login" component={LoginPage} />
         <Route path="/admin" component={AdminFeature} />
-        <Route path="/" exact component={Product}/>
-        <Route path="/productDetails" exact component={ProductDetail}/>
-        <Route path="/:somestring" component={ErrorPage}/>
+        <Route path="/" exact component={Product} />
+        <Route path="/productDetails" exact component={ProductDetail} />
+        <Route path="/:somestring" component={ErrorPage} />
       </Switch>
     </div>
   );
