545e0737e2661991e28e96cc8ae611282c8abbcd
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-07-16 23:14:45 +0700

    Setup api for product and category

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -30,6 +30,26 @@ const productApi = {
       },
     };
   },
+
+  get(id) {
+    const url = `/products/:${id}`;
+    return axiosClient.get(url);
+  },
+
+  add(data) {
+    const url = `/products/:${data.id}`;
+    return axiosClient.post(url, data);
+  },
+
+  update(data) {
+    const url = `/products/:${data.id}`;
+    return axiosClient.patch(url, data);
+  },
+
+  remove(id) {
+    const url = `/products/:${id}`;
+    return axiosClient.delete(url);
+  },
 };
 
 export default productApi;
