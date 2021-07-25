79b5361ac41a0f3ab0eb34a36b987ac869f08dd3
lương lê <68262875+manred49@users.noreply.github.com>
2021-07-25 14:28:31 +0700

    update

--- src/features/ProductDetail/components/ProductInfor/ProductInfor.jsx
+++ src/features/ProductDetail/components/ProductInfor/ProductInfor.jsx
@@ -1,66 +1,73 @@
-import styles from '../../ProductDetail.module.css'
-import React,{ useEffect,useState } from 'react';
+import styles from '../../ProductDetail.module.css';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { formatPrice } from 'utils';
 import { useParams } from 'react-router-dom';
 function ProductInfor(props) {
-    const [product, setproduct] = useState(null);
-    const { id } = useParams();
-   
+  const [product, setproduct] = useState(null);
+  const { id } = useParams();
+
   useEffect(() => {
     if (id) {
-        const getApi = `https://api-mts.herokuapp.com/products/${id}`;
-        axios.get(getApi).then((response) => {
-            setproduct(response.data);
-        });
+      const getApi = `https://api-mts.herokuapp.com/products/${id}`;
+      axios.get(getApi).then((response) => {
+        setproduct(response.data);
+      });
     }
-}, [id]);
-return (
+  }, [id]);
+  return (
     <div>
-            {console.log(product)}
-            <h4 className={styles.ProductName}>{product?.productName}</h4>
-            <h5 className={styles.ProductBand}> 
-            {/* <i class="fab fa-apple"></i> */}
-            {product?.brand}
-            </h5>
-            <p className={styles.ProductDescription}>{product?.productDescription}</p>
-            <div className={styles.ProductTable}>
-                <div className={styles.ProductTableRow}>
-                    <span className={styles.ProductItem}>Brand</span>
-                    <span className={styles.ProductItem}>{product?.brand}</span>
-                </div>
-                <div className={styles.ProductTableRow}>
-                    <span className={styles.ProductItem}>Loại</span>
-                    <span className={styles.ProductItem}>{product?.category.categoryName}</span>
-                </div>
-                <div className={styles.ProductTableRow}>
-                    <span className={styles.ProductItem}>Nước</span>
-                    <span className={styles.ProductItem}>USA</span>
-                </div>
-                <div className={styles.ProductTableRow}>
-                    <span className={styles.ProductItem}>Số lượng mua</span>
-                    <span className={styles.ProductItem}>23.000</span>
-                </div>
-               
-            </div>
-            <div className={styles.productQuantity}>
-                <div className={styles.quantityTitle}>Số lượng</div>
-                <span className={styles.quantityBox}>
-                    <i class="fas fa-minus"></i>
-                    <span className={styles.numberQuantity}>
-                        1
-                    </span>
-                    <i class="fas fa-plus"></i>
-                </span>
-            </div>
-            <div className={styles.ProductCartWapper}>
-                <div className={styles.ProductPriceWapper}>{formatPrice(product?.salePrice)}</div>
-                <button className={styles.btn_cart}>
-                <i class="fas fa-shopping-cart"></i>
-                Cart</button>
-            </div>
+      {console.log(product)}
+      <h4 className={styles.ProductName}>{product?.productName}</h4>
+      <h5 className={styles.ProductBand}>
+        {/* <i class="fab fa-apple"></i> */}
+        {product?.brand}
+      </h5>
+      <p className={styles.ProductDescription}>{product?.productDescription}</p>
+      <div className={styles.ProductTable}>
+        <div className={styles.ProductTableRow}>
+          <span className={styles.ProductItem}>Brand</span>
+          <span className={styles.ProductItem}>{product?.brand}</span>
         </div>
-    );
+        <div className={styles.ProductTableRow}>
+          <span className={styles.ProductItem}>Loại</span>
+          <span className={styles.ProductItem}>
+            {product?.category.categoryName}
+          </span>
+        </div>
+        <div className={styles.ProductTableRow}>
+          <span className={styles.ProductItem}>Nước</span>
+          <span className={styles.ProductItem}>USA</span>
+        </div>
+        <div className={styles.ProductTableRow}>
+          <span className={styles.ProductItem}>Số lượng mua</span>
+          <span className={styles.ProductItem}>23.000</span>
+        </div>
+      </div>
+      <div className={styles.productQuantity}>
+        <div className={styles.quantityTitle}>Số lượng</div>
+        <span className={styles.quantityBox}>
+          <i class="fas fa-minus"></i>
+          <input
+            type="text"
+            className={styles.numberQuantity}
+            name="productName_contains"
+            placeholder=""
+          />
+          <i class="fas fa-plus"></i>
+        </span>
+      </div>
+      <div className={styles.ProductCartWapper}>
+        <div className={styles.ProductPriceWapper}>
+          {formatPrice(product?.salePrice)}
+        </div>
+        <button className={styles.btn_cart}>
+          <i class="fas fa-shopping-cart"></i>
+          Cart
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ProductInfor;
