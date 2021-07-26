fef25e1c80777b80259f84561d27f93a59422aae
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-07-26 10:11:16 +0700

    Make cart feature

---
+++ src/features/Cart/components/ProductTotal.jsx
@@ -0,0 +1,56 @@
+import { Box, Grid, IconButton, makeStyles, Paper } from '@material-ui/core';
+import { DeleteOutlined } from '@material-ui/icons';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { cartItemsCountSelector } from '../selectors';
+
+ProductTotal.propTypes = {};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    paddingLeft: theme.spacing(2),
+
+    fontSize: '14px',
+  },
+
+  center: {
+    textAlign: 'center',
+  },
+}));
+
+function ProductTotal() {
+  const cartItemsCount = useSelector(cartItemsCountSelector);
+  const classes = useStyles();
+
+  return (
+    <Box>
+      <Paper elevation={0}>
+        <Grid container className={classes.root}>
+          <Grid item lg={5}>
+            {`Tất cả ${!isNaN(cartItemsCount) ? cartItemsCount : 0} sản phẩm`}
+          </Grid>
+          <Grid item lg={2} className={classes.center}>
+            Đơn giá
+          </Grid>
+          <Grid item lg={2} className={classes.center}>
+            Số lượng
+          </Grid>
+          <Grid item lg={2} className={classes.center}>
+            Thành tiền
+          </Grid>
+          <Grid item lg={1} className={classes.center}>
+            <IconButton>
+              <DeleteOutlined />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Box>
+  );
+}
+
+export default ProductTotal;
--- src/features/Cart/index.jsx
+++ src/features/Cart/index.jsx
@@ -1,10 +1,76 @@
+import {
+  Box,
+  Container,
+  Grid,
+  makeStyles,
+  Paper,
+  Typography,
+} from '@material-ui/core';
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeFromCart, setQuantity } from './cartSlice';
+import DetailCart from './components/DetailCart';
+import ProductTotal from './components/ProductTotal';
+import TotalCost from './components/TotalCost';
 
 CartFeature.propTypes = {};
 
-function CartFeature(props) {
-  return <div>Cart</div>;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(3),
+  },
+  left: {
+    width: '920px',
+    paddingRight: theme.spacing(1.5),
+  },
+  right: {
+    width: '310px',
+    padding: theme.spacing(1.5),
+  },
+}));
+
+function CartFeature() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const handleRemoveFromCart = (productId) => {
+    const action = removeFromCart(productId);
+    dispatch(action);
+  };
+
+  const handleChangeQuantity = (product) => {
+    const action = setQuantity(product);
+    dispatch(action);
+  };
+
+  return (
+    <Box className={classes.root}>
+      <Container>
+        <Typography
+          component="h1"
+          variant="h5"
+          style={{ marginBottom: '12px' }}
+        >
+          GIỎ HÀNG
+        </Typography>
+        <Grid container>
+          <Grid item className={classes.left}>
+            <ProductTotal />
+            <DetailCart
+              onRemove={handleRemoveFromCart}
+              onChange={handleChangeQuantity}
+            />
+          </Grid>
+          <Paper elevation={0}>
+            <Grid item className={classes.right}>
+              <TotalCost />
+            </Grid>
+          </Paper>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
 
 export default CartFeature;
